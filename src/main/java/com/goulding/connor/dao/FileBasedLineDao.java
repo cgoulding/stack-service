8142593a1249914c2d5c0c47648257c6d4ee6bab
Connor Goulding <connorg_2000@hotmail.com>
2017-02-21 08:09:18 +0000

    Line service

---
+++ src/main/java/com/goulding/connor/dao/FileBasedLineDao.java
@@ -0,0 +1,58 @@
+package com.goulding.connor.dao;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goulding.connor.domain.Line;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by root on 20/02/17.
+ */
+public class FileBasedLineDao implements LineDao {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileBasedLineDao.class);
+
+    private final String location;
+    private final ObjectMapper objectMapper;
+
+    public FileBasedLineDao(final String location, final ObjectMapper objectMapper) {
+        this.location = location;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void addLine(Line line) {
+        List<Line> current = readFromFile();
+        current.add(line);
+        saveToFile(current);
+    }
+
+    @Override
+    public List<Line> readLines() {
+        return readFromFile();
+    }
+
+    private List<Line> readFromFile() {
+        try (FileInputStream inputStream = new FileInputStream(location)) {
+            return Arrays.asList(objectMapper.readValue(inputStream, Line[].class));
+        } catch (IOException exception) {
+            LOGGER.error("Unable to read from file", exception);
+        }
+        return new ArrayList<>();
+    }
+
+    private void saveToFile(List<Line> lines) {
+        try (FileOutputStream outputStream = new FileOutputStream(location)) {
+            objectMapper.writeValue(outputStream, lines);
+        } catch (IOException exception) {
+            LOGGER.error("Unable to write to file", exception);
+        }
+    }
+}
---
+++ src/main/java/com/goulding/connor/service/LineService.java
@@ -0,0 +1,7 @@
+package com.goulding.connor.service;
+
+/**
+ * Created by root on 20/02/17.
+ */
+public interface LineService {
+}
